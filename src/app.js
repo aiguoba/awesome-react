92d4fd0d36bd359e6ccf9f1270d28245a5ea8d68
Day <874288011@qq.com>
2018-12-10 15:54:02 +0800

    rename folder name

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import { observer } from 'mobx-react'
-import Test from '~/components/test'
+import MobxTest from '~/components/mobx-test'
 import Store from './store'
 const store = new Store()
 
 @observer
 export default class App extends Component {
   render() {
-    return <Test store={store} />
+    return <MobxTest store={store} />
   }
 }
